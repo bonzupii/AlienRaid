643538f2dd67a8bd97fb14ab577ed64f31ad9a78
bonzupii <bonzupii@protonmail.com>
2019-05-18 21:47:26 -0500

    Added a script to automatically install dependencies on Debian and Ubuntu.

--- installdeps-apt.sh
+++ installdeps-apt.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-echo "Does your system use the apt package management system? (Y/N)"
-read response
-if [[ $response == [yY]* ]]; then
-    declare -a packages=("python3-pip" "libsdl2-dev" "libpython3-dev")
-else
-    echo "This dependency installer only works on systems with the apt package management system. Aborting."
-    exit
-fi
+echo "Checking for apt package manager..."
+command -v apt >/dev/null 2>&1 || { echo >&2 "Apt package manager not found. This dependency installer only works on systems with the apt package management system. Aborting."; exit 1; }
+echo "Apt package manager found. Installation can continue."
+declare -a packages=("python3-pip" "libsdl2-dev" "libpython3-dev")
 for i in "${packages[@]}"; do
     if ! dpkg-query -W -f='${Status}' $i 2>/dev/null | grep -q "ok installed"; then
         echo "$i is not installed, would you like to install it now? (Y/N)"
@@ -25,5 +21,5 @@ done
 echo "Updating pip3 setuptools..."
 sudo pip3 install --upgrade setuptools
 echo "Installing python libtcod..."
-sudo pip3 install tcod
+pip3 install --user tcod
 
